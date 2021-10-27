303871a19737b4739cade4a4f4fc39826a696e31
Mayer Quentin <93088506+quentair@users.noreply.github.com>
2021-10-27 15:44:14 +0200

    Update cocktail.php et navigation.php
    Parcours des branches du site et affichage dynamique des coktails.

--- Controller/navigation.php
+++ Controller/navigation.php
@@ -1,23 +1,46 @@
-<?php  include("../Data/Donnee/Donnees.inc.php")?> 
+<?php  include("../Data/Donnee/Donnees.inc.php") ?> 
+
+<?php
+//extraction d'un argument voulu depuis l'url
+function extraire_arg($arg_voulu)
+{
+    $arg="";
+   if(isset($_GET[$arg_voulu])) {
+        $arg = $_GET[$arg_voulu];
+   }
+   return $arg;
+}
+
+?>
 
 <nav>
 
     <label>Aliment Courant</label>
 
-    <a href="#">Aliment</a>
+    <a href=".#">Aliment</a>
 
     <label>Sous-catégories :</label>
     <!-- debut de generation PHP -->
     <?php
 
+    //on prend soit l'aliment de l'url soit l'aliment par defaut (Aliment) si pas défini
+    $aliment = extraire_arg("alimentcourant");
+    if($aliment == "") {
+        $aliment = "Aliment";
+    }
+
     echo "<ul>";
-    foreach($Hierarchie['Aliment'] as $TabIngr)
+    
+    //pour chaque ingredient associé a notre aliment courant, si celui-ci fait bien parti d'une sous-categorie
+    foreach($Hierarchie[$aliment] as $key => $categorie)
     {
-        foreach($TabIngr as $ingr)
-        {
-            echo "<li>";
-            echo "<a href='alimentcourant=$ingr'>$ingr</a>";
-            echo "</li>";
+        if($key == "sous-categorie") {
+            foreach($categorie as $ingr)
+            {
+                echo "<li>";
+                echo "<a href='?alimentcourant=$ingr'>$ingr</a>";
+                echo "</li>";
+            }
         }
     }
     echo "</ul>";
