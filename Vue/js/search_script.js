6ab0ee8cd41ba6720a3a8a8269b2070b5bb2e125
Mayer Quentin <93088506+quentair@users.noreply.github.com>
2021-12-02 15:26:00 +0100

    Update script recherche

--- Vue/js/search_script.js
+++ Vue/js/search_script.js
@@ -1,1 +1,16 @@
+/*const form = document.getElementById('form_recherche');*/
+const boutton = document.getElementById('button_search');
 
+boutton.addEventListener('click', function() {
+    // handle the form data
+    const barre_recherche = document.getElementById('recherche');
+    //const boutton = document.getElementById['button_search'];
+
+    var recherche = barre_recherche.value;
+    recherche = recherche.replace('+', '*')
+    var url = "../Vue/index.php?page=recherche&search=";
+    url = url.concat(recherche);
+
+    location.assign(url);
+    return false;
+}, false);
